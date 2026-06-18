579cfe260ef539ace782ddc2c8ab49bdbed8f589
pinki-12 <pk0540080@gmail.com>
2026-06-18 11:27:03 +0530

    thrid commit

--- src/pages/AI/AIChat.jsx
+++ src/pages/AI/AIChat.jsx
@@ -23,7 +23,7 @@ const AIChat = () => {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/ai/ask",
+        "https://omni-chats-backend.onrender.com/api/ai/ask",
         {
           message: currentMessage,
         }
